5c148918d93096a0ad5fade5d3dd5c987c7b719b
AntoineCouck <74922305+AntoineCouck@users.noreply.github.com>
2022-01-22 10:51:23 +0100

    dashboard finished

--- app/src/main/java/com/example/android_examen_couckantoine/MainActivity.java
+++ app/src/main/java/com/example/android_examen_couckantoine/MainActivity.java
@@ -23,12 +23,10 @@ public class MainActivity extends AppCompatActivity {
         setContentView(binding.getRoot());
 
         BottomNavigationView navView = findViewById(R.id.nav_view);
-        // Passing each menu ID as a set of Ids because each
-        // menu should be considered as top level destinations.
+
         AppBarConfiguration appBarConfiguration = new AppBarConfiguration.Builder(
                 R.id.navigation_incomes, R.id.navigation_d, R.id.navigation_expenses)
                 .build();
-
 
 
         NavHostFragment navHostFragment =
@@ -37,7 +35,7 @@ public class MainActivity extends AppCompatActivity {
         assert navHostFragment != null;
         NavController navController = navHostFragment.getNavController();
 
-//        NavController navController = Navigation.findNavController(this, R.id.nav_host_fragment_activity_main);
+
         NavigationUI.setupActionBarWithNavController(this, navController, appBarConfiguration);
         NavigationUI.setupWithNavController(binding.navView, navController);
     }
